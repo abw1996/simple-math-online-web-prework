30b044abead249169f20f400b7f9cd7bb599e99f
Ashton Williams <ashtonwilliams1996@gmail.com>
2019-04-18 01:11:21 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -15,7 +15,7 @@ def multiplication(num1, num2)
 end
 
 def modulo(num1, num2)
-  return
+  return num1 % num2
 end
 
 def square_root(num)
@@ -23,5 +23,5 @@ def square_root(num)
 end
 
 def order_of_operation(num1, num2, num3, num4)
-  #Hint:  __ + (( __ * __ ) / __ )
+  return  num1 + ((num2 * num3 ) / num4 )
 end
